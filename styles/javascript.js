e401674f095d7b482bf9c9abdee8d324ba4d1839
Inon Bercu <inonbr@gmail.com>
2020-01-07 15:55:40 +0200

    done most of js

--- styles/javascript.js
+++ styles/javascript.js
@@ -1,11 +1,14 @@
 const noRobot = document.querySelector('.g-recaptcha');
 const submitBtn = document.getElementById('submit');
+const resetBtn = document.getElementById('reset');
 
 const onNoRobotClick = () => {
   console.log('hello world!')
   submitBtn.addEventListener('click', (event) => {
     console.log('hello world one more time!')
     event.preventDefault();
+    resetBtn.style.display = "none";
+    submitBtn.style.display = "none";
     noRobot.style.display = "block";
   });
 };
